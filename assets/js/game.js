1b75f358e51e0e86d30b15a1f9613a7b682d5eae
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-11-01 17:40:20 -0300

    player e timer

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid')
+const spanPlayer= document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'beth',
@@ -26,7 +28,8 @@ const chackEndGame = ()=>{
     const disabledCards = document.querySelectorAll('.disabled-card');
 
     if(disabledCards.length ===20){
-        alert('Parabéns, Você Ganhou!!!')
+        clearInterval(this.loop);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`)
     }
 }
 
@@ -101,4 +104,24 @@ const loadGame = ()=>{
     })
 }
 
-loadGame();
+const startTimer = () => {
+
+
+    this.loop = setInterval(()=>{
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+
+    },1000);
+}
+
+window.onload = () => {
+
+    spanPlayer.innerHTML = localStorage.getItem('player');
+    
+    startTimer();
+    loadGame();
+
+
+}
+
+
